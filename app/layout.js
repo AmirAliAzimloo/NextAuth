40e0b17b6e8eddc2bf638331cab96bf60237f660
Amir Ali <amiraliazimloo123@gmail.com>
2023-10-04 23:08:19 +0330

    next-auth provider & config for github completed

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import Header from '@/components/header'
 import '../styles/globals.css';
 import { Inter } from 'next/font/google'
-
+import NextAuthProvider from './providers/NextAuthProvider';
 
 export const metadata = {
   title: 'next-auth',
@@ -12,8 +12,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className=" container mx-auto my-8 flex flex-col gap-6 " >
+        <NextAuthProvider>
         <Header />
-        {children}</body>
+        {children}
+        </NextAuthProvider>
+       
+        </body>
     </html>
   )
 }
